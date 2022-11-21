1c2ba754e989e8cde1be880144f045f586bce8a7
Xavski <xavioursaveski1@gmail.com>
2022-11-22 07:58:25 +1100

    Functional New Items Section

---
+++ src/components/UI/CountdownTimer.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+let time;
+
+const CountdownTimer = ({ expiryDate }) => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const deadline = expiryDate;
+
+  const getTime = () => {
+    time = deadline - Date.now();
+
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    
+      <>
+      {
+        time < 0 ? 'EXPIRED' : <> {hours + 'h ' + minutes + 'm ' + seconds + 's'}</>
+      }
+      </>
+  );
+};
+
+export default CountdownTimer;
+
+//maybe try using a prop instead
